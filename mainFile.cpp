54641334a17d67ab32af79ebbaf0b538693d433a
theeldestelder <joshuag98@gmail.com>
2017-07-23 22:09:50 -0500

    add README and first code test

---
+++ mainFile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string email = "";
+
+	cout << "Enter your email address:";
+	cin >> email;
+
+	cout << "Your email is: " << email << endl;
+}
