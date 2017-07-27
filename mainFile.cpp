dc1fdf9401060cd5df8c9755bf82e71c0ca45435
theeldestelder <joshuag98@gmail.com>
2017-07-26 22:19:58 -0500

    Initial version of addPeriods
    Program prints all iterations of the username with periods in every position such that
    every iteration contains one and only one period

--- mainFile.cpp
+++ mainFile.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 string getUsername(string input); // returns all text before '@' in email.  assumed only 1 '@' in email.
+void addPeriods(string input); // void for now
 
 int main()
 {
@@ -11,8 +12,12 @@ int main()
 	cout << "Enter your email address: ";
 	cin >> email;
 
-	cout << getUsername(email) << endl;;
-	
+	//cout << getUsername(email) << endl;;
+
+	string username = getUsername(email);
+//	cout << username << endl;
+
+	addPeriods(username);
 }
 
 string getUsername(string input)
@@ -28,4 +33,16 @@ string getUsername(string input)
 	*/
 
 	return input.substr(0, input.find('@'));
+}
+
+void addPeriods(string input) // takes an input string and inserts periods at every possible space in the input string
+{
+	int length = input.length();
+	string original = input;
+
+	for(int i = 1; i < length; i++) // start after first character
+	{
+		cout << input.insert(i, ".") << endl;
+		input = original;
+	}
 }
